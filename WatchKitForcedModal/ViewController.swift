f40c4c1cc590c3723c8120c4effca309b14b078f
Ben Chatelain <benchatelain@gmail.com>
2015-05-12 09:43:59 -0600

    Initial Commit

---
+++ WatchKitForcedModal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitForcedModal
+//
+//  Created by Ben Chatelain on 5/12/15.
+//  Copyright (c) 2015 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
